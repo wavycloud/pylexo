74956574eec4d4f8b353a14044cb4091bdda8e00
Gehad Shaat <gehad.shaath@gmail.com>
2018-01-28 18:06:30 -0800

    jsonobject is required

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pylexo',
-      version='1.1',
+      version='1.2',
       packages=find_packages(),
       description='Pythonic Lex Object (PyLexO)',
       author='WavyCloud',
@@ -13,7 +13,7 @@ setup(name='pylexo',
       },
       url='https://github.com/wavycloud/pylexo',
       py_modules=['pylexo'],
-      install_requires=[],
+      install_requires=['jsonobject'],
       license='MIT License',
       zip_safe=True,
       keywords='aws python lex lambda',
